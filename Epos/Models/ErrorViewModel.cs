d6119e4944462abbae68e560f913e6c39552ae57
FeziFazeel <heartzkiller786@gmail.com>
2018-02-26 15:14:38 +0500

    Add project files.

---
+++ Epos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Epos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
